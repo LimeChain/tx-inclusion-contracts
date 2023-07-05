f9dcce896be884196a5ceca013ec2d6d8499ad9b
Ivan Kyuchukov <vaka.stz@gmail.com>
2023-07-05 16:29:19 +0300

    remove DummyRefunder contract

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import {TrustedOracle} from "../src/TrustedOracle.sol";
 import {TransactionInclusionProver} from "../src/TransactionInclusionProver.sol";
-import {DummyRefunder} from "../src/DummyRefunder.sol";
 
 contract DeployScript is Script {
     function setUp() public {}
@@ -17,7 +16,6 @@ contract DeployScript is Script {
         TransactionInclusionProver prover = new TransactionInclusionProver(
             address(oracle)
         );
-        DummyRefunder refunder = new DummyRefunder(address(prover));
 
         vm.stopBroadcast();
     }
--- test/ReceiptInclusionProver.t.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "forge-std/Test.sol";
-import "../src/TransactionInclusionProver.sol";
-
-contract TransactionInclusionProverTest is Test {
-    function setUp() public {}
-}
